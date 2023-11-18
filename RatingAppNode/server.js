3ea802e3a18a1f8931175d29a642f470ff83f52b
Julian Hellstern <jh-213777@hs-weingarten.de>
2023-11-18 15:22:34 +0100

    alternativer code

--- RatingAppNode/server.js
+++ RatingAppNode/server.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const app = express();
-const fs = require("fs");
+const {MongoClient} = require("mongodb");
 const cors = require("cors");
 const port = 8080;
-const filename = __dirname + "/profs.json";
+
+const mongoURI = "mongodb://localhost:27017";
+const dbName = "prof-rating-app";
+const collectionName = "profs";
+
 
 //Middleware test
 app.use(express.json()); //for parsing application/json
@@ -14,69 +18,71 @@ function log(req, res, next) {
 }
 app.use(log);
 
+let db;
+
+async function connectToDatabase() {
+    const client = new MongoClient(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
+    try {
+        await client.connect();
+        console.log("Connected to the database");
+        db = client.db(dbName);
+    } catch (err) {
+        console.error("Error connecting to the database:", err);
+    }
+}
+
+connectToDatabase();
 
 //Endpoints
-app.get("/profs", function (req, res) {
-    fs.readFile(filename, "utf8", function (err, data) {
-        res.writeHead(200, {
-            "Content-Type": "application/json",
-        });
-        res.end(data);
-    });
+app.get("/profs", async function (req, res) {
+    const collection = db.collection(collectionName);
+    const data = await collection.find({}).toArray();
+    res.json(data);
 });
 
-app.get("/profs/:id", function (req, res) {
-    fs.readFile(filename, "utf8", function (err, data) {
-        const dataAsObject = JSON.parse(data)[req.params.id];
-        res.writeHead(200, {
-            "Content-Type": "application/json",
-        });
-        res.end(JSON.stringify(dataAsObject));
-    });
+app.get("/profs/:id", async function (req, res) {
+    const collection = db.collection(collectionName);
+    const data = await collection.findOne({ _id: parseInt(req.params.id) });
+    res.json(data);
 });
 
-app.put("/profs/:id", function (req, res) {
-    fs.readFile(filename, "utf8", function (err, data) {
-        let dataAsObject = JSON.parse(data);
-        dataAsObject[req.params.id].name = req.body.name;
-        dataAsObject[req.params.id].rating = req.body.rating;
-        fs.writeFile(filename, JSON.stringify(dataAsObject), () => {
-            res.writeHead(200, {
-                "Content-Type": "application/json",
-            });
-            res.end(JSON.stringify(dataAsObject));
-        });
-    });
+app.put("/profs/:id", async function (req, res) {
+    const collection = db.collection(collectionName);
+    const updatedProf = {
+        name: req.body.name,
+        rating: req.body.rating,
+    };
+    const result = await collection.updateOne({ _id: parseInt(req.params.id) }, { $set: updatedProf });
+    res.json({ message: `${result.modifiedCount} prof(s) updated` });
+    
 });
 
-app.delete("/profs/:id", function (req, res) {
-    fs.readFile(filename, "utf8", function (err, data) {
-        let dataAsObject = JSON.parse(data);
-        dataAsObject.splice(req.params.id, 1);
-        fs.writeFile(filename, JSON.stringify(dataAsObject), () => {
-            res.writeHead(200, {
-                "Content-Type": "application/json",
-            });
-            res.end(JSON.stringify(dataAsObject));
-        });
-    });
+app.delete("/profs/:id", async function (req, res) {
+    const collection = db.collection(collectionName);
+
+    const result = await collection.deleteOne({ _id: parseInt(req.params.id) });
+    res.json({ message: `${result.deletedCount} prof(s) deleted` });
 });
 
-app.post("/profs", function (req, res) {
-    fs.readFile(filename, "utf8", function (err, data) {
-        let dataAsObject = JSON.parse(data);
-        dataAsObject.push({
-            id: dataAsObject.length,
-            name: req.body.name,
-            rating: req.body.rating,
-        });
-        fs.writeFile(filename, JSON.stringify(dataAsObject), () => {
-            res.writeHead(200, {
-                "Content-Type": "application/json",
-            });
-            res.end(JSON.stringify(dataAsObject));
-        });
-    });
+app.post("/profs", async function (req, res) {
+    const collection = db.collection(collectionName);
+    const newProf = {
+        name: req.body.name,
+        rating: req.body.rating,
+    };
+
+    try {
+        const result = await collection.insertOne(newProf);
+
+        if (result.ops && result.ops.length > 0) {
+            res.json(result.ops[0]);
+        } else {
+            res.status(500).json({ error: "Failed to insert document" });
+        }
+    } catch (error) {
+        console.error("Error inserting document:", error);
+        res.status(500).json({ error: "Failed to insert document" });
+    }
 });
 
 app.listen(port, () => console.log(`Server listening on port ${port}!`));
